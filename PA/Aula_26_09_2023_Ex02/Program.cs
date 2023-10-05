387a233dde1df6fa75e48fd381cb25a411b54165
Aluno <aluno@etec.sp.gov.br>
2023-10-05 20:51:20 -0300

    Atividade Aula 05/10/2023

---
+++ PA/Aula_26_09_2023_Ex02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Aula_26_09_2023_Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float num;
+
+            Console.Write("Digite um numero");
+            num = float.Parse(Console.ReadLine());
+
+            if
+
+
+
+
+
+        }
+    }
+}
---
+++ PA/Aula_5_10_2023_Ex01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Aula_5_10_2023_Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            float valor1, valor2;
+            string operador;
+
+            Console.Write("Informe o 1º valor");
+            valor1 = float.Parse(Console.ReadLine()); 
+
+            Console.Write("Informe o 2º valor");
+            valor2 = float.Parse(Console.ReadLine());
+
+            Console.Write("Informe o operador");
+            operador = (Console.ReadLine());
+
+            if (operador == "+")
+            {
+
+                Console.WriteLine($"valor da Soma é {valor1 + valor2}");
+            }
+
+            else if (operador == "-")
+            {
+                Console.WriteLine($"valor da Subtração é {valor1 - valor2}");
+            }
+            else if (operador == "*")
+            {
+                Console.WriteLine($"valor da Multiplicação é {valor1 * valor2}");
+            }
+            else
+            {
+                Console.WriteLine($"valor da subtração é {valor1 / valor2}");
+            }
+        }
+    }
+}
