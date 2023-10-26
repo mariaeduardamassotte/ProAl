1beb94196ddfa3590e20de88730a70eec8b54e5b
Aluno <aluno@etec.sp.gov.br>
2023-10-26 20:49:26 -0300

    Aula_26_10_2023

---
+++ PA/Aula_26_10_2023_Ex02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Aula_26_10_2023_Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float valorFinanciado, valorParcela;
+            int qtdeParcela;
+            int contador = 1;
+
+            Console.Write("Informe o valor Financiado: ");
+            valorFinanciado = float.Parse(Console.ReadLine());
+
+            Console.Write("Informe a Quantidade de Parcelas: ");
+            qtdeParcela = int.Parse(Console.ReadLine());
+
+            valorParcela = valorFinanciado / qtdeParcela;
+
+            DateTime data = DateTime.Now.Date;
+
+            while (contador <= qtdeParcela)
+            {
+                // Console.WriteLine($"O valor da parcela {contador} é de {valorParcelas");
+                Console.WriteLine($"{contador}ª parcela: ({data}){valorParcela:0.00}");
+                //Console.WriteLine($"{contador}ª parcela: {valorFinanciado / valorParcela:0.00}");
+                contador++;
+                data = data.AddMonths(1);
+
+            }
+        }
+    }
+}
